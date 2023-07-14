170016a382d931aacfcc4612c8e8f862628c3231
Erick Hilario <erprogramador@gmail.com>
2023-07-14 15:56:33 -0500

    feature: replicate error in react-datetime-picker package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DateTimePicker from 'react-datetime-picker';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
   return (
@@ -10,14 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DateTimePicker
+          onChange={()=> {}}
+          value={new Date("12/12/2020")}
+          clearIcon={null}
+          maxDate={new Date(3000, 12, 30)}
+        />
       </header>
     </div>
   );
